caf9cd79f42789b704a224e2c20a899d4bdeec68
sswargam1234 <129363104+sswargam1234@users.noreply.github.com>
2025-09-14 01:39:55 -0500

    error codes added

--- src/main/java/com/vehicle/exception/GlobalExceptionHandler.java
+++ src/main/java/com/vehicle/exception/GlobalExceptionHandler.java
@@ -4,7 +4,7 @@ import jakarta.persistence.EntityNotFoundException;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.FieldError;
@@ -24,13 +24,12 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
                                                                   HttpHeaders headers,
-                                                                  HttpStatus status,
+                                                                  HttpStatusCode status,
                                                                   WebRequest request) {
         List<String> errors = ex.getBindingResult().getFieldErrors()
                 .stream()
                 .map(FieldError::getDefaultMessage)
                 .collect(Collectors.toList());
-
         ErrorResponse body = new ErrorResponse(status.value(), "Validation Failed", "Validation errors occurred", request.getDescription(false), errors);
         return new ResponseEntity<>(body, status);
     }
